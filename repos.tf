0b53d238b1174de42fb6d5e0445e4d3fb1295c4c
alvarof2 <fernandez.f.alvaro@gmail.com>
2022-05-09 17:38:28 +0200

    Remove branch protection

--- repos.tf
+++ repos.tf
@@ -15,17 +15,3 @@ resource "github_repository_file" "terraform_github_readme" {
   commit_email        = "terraform@example.com"
   overwrite_on_create = true
 }
-
-resource "github_branch_protection" "terraform_github_main" {
-  repository_id = github_repository.terraform_github.node_id
-
-  pattern          = "main"
-  enforce_admins   = true
-  allows_deletions = true
-
-  required_pull_request_reviews {
-    required_approving_review_count = 1
-  }
-
-  depends_on = [github_repository_file.terraform_github_readme]
-}
